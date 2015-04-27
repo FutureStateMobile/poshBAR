b0fbf0a6ca2541c201dba3a05d2c01a448e4ebdf
Chase Florell <chase_florell@transcanada.com>
2015-04-27 11:48:08 -0600

    [CF] fixed bug in test coverage

--- src/poshBAR/xUnit.ps1
+++ src/poshBAR/xUnit.ps1
@@ -1,3 +1,23 @@
+<#
+    .SYNOPSIS
+        Runs XUnit against a test dll
+    .DESCRIPTION
+        
+    .PARAMETER targetAssembly
+        The target test assembly
+    .PARAMETER outputDir
+        The directory to store the output
+    .PARAMETER runCommand
+        The namespace to start the tests from
+    .PARAMETER includeCoverage
+        Switch to tell if we want coverage on the tests
+    .PARAMETER coverageRulesPath
+        Path to coverage rules
+    .EXAMPLE
+        Invoke-Nunit "$buildDir\myTestAssembly.dll" "$outputDir" "myAssembly.Unit" -includeCoverage "$coverageRulesPath"
+    .NOTES
+        Currently 'includeCoverage' invokes the DotCover coverage tool. Some day we'll need to re-think how we want to achieve this.
+#>
 function Invoke-XUnit{
     [CmdletBinding()]
     param( 
@@ -19,6 +39,24 @@ function Invoke-XUnit{
     }
 }
 
+<#
+    .SYNOPSIS
+        Runs XUnit against a test dll
+    .DESCRIPTION
+        
+    .PARAMETER targetAssembly
+        The target test assembly
+    .PARAMETER outputDir
+        The directory to store the output
+    .PARAMETER runCommand
+        The namespace to start the tests from
+    .PARAMETER coverageRulesPath
+        Path to coverage rules
+    .EXAMPLE
+        Invoke-XUnitWithCoverage "$buildDir\myTestAssembly.dll" "$outputDir" "myAssembly.Unit" "$coverageRulesPath"
+    .NOTES
+        Currently 'includeCoverage' invokes the DotCover coverage tool. Some day we'll need to re-think how we want to achieve this.
+#>
 function Invoke-XUnitWithCoverage {
     [CmdletBinding()]
     param( 
@@ -32,18 +70,9 @@ function Invoke-XUnitWithCoverage {
     $xmlFile = "$fileName-TestResults.xml"
     $txtFile = "$fileName-TestResults.txt"
     $coverageFile = "$fileName-CoverageResults.dcvr"
-
-    $coverageConfig = Get-ConfigFile $targetAssembly
     
     # who knows, this might fall over one day.
     $xu = resolve-path ".\..\packages\xunit.runners.*\tools\xunit-console.exe"
-    exec{ dotcover.exe cover $coverageConfig /TargetExecutable=$xu /TargetArguments="$targetAssembly /fixture:$runCommand /xml=$xmlFile /out=$txtFile /nologo /framework=4.0" /Output=$coverageFile /ReportType=html} ($msgs.error_coverage_failed -f $runCommand)
+    exec{ dotcover.exe cover $coverageRulesPath /TargetExecutable=$xu /TargetArguments="$targetAssembly /fixture:$runCommand /xml=$xmlFile /out=$txtFile /nologo /framework=4.0" /Output=$coverageFile /ReportType=html} ($msgs.error_coverage_failed -f $runCommand)
     $msgs.msg_teamcity_importdata -f 'dotNetCoverage', 'dotcover', $coverageFile
-}
-
-
-function Get-ConfigFile ($input) {
-    $x = $input -replace ".dll", ".config"
-    $x = $x -replace ".", "-"
-    return $x
 }
