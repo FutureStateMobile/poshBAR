354006b708f135f8a3e094259ae66a703301d25b
Chase Florell <chase_florell@transcanada.com>
2015-05-29 09:50:51 -0600

    [CF] just a few more tests

--- tests/Find-ToolPath.Tests.ps1
+++ tests/Find-ToolPath.Tests.ps1
@@ -1,33 +1,46 @@
-$ErrorActionPreference = "Stop"
+$ErrorActionPreference = 'Stop'
     
-Describe "Find-ToolPath" { 
-    Context "Handle a valid tool."{
+Describe 'Find-ToolPath' { 
+    Context 'Handle a valid tool.'{
         # setup
         $toolName = 'mage'
+        
+        
+        # execute
+        $execute = {$result = Find-ToolPath $toolName} 
             
         # assert
-        It "Will not thow for a valid tool." {
-            {Find-ToolPath $toolName} | should not throw
+        It 'Will not thow for a valid tool.' {
+            $execute | should not throw
         }
         
-        # assert
-        It "Will execute the tool and not throw an exception." {
+        It 'Will execute the tool and not throw an exception.' {
            {. $toolName -h} | should not throw 
+        }
+        
+        It 'Will have a valid path on the result.' {
+            $result | should not be ''
         }
     }
     
-    Context "Handle an invalid tool." {
+    Context 'Handle an invalid tool.' {
         # setup
         $toolName = 'Foo'
+            
+        # execute
+        $execute = {$result = Find-ToolPath $toolName}
 
         # assert
-        It "Will throw on an invalid tool." {
-            {Find-ToolPath $toolName} | should throw
+        It 'Will throw on an invalid tool.' {
+            $execute | should throw
         }
 
-        # assert
-        It "Will throw an exception when attempting to execute an invalid tool." {
+        It 'Will throw an exception when attempting to execute an invalid tool.' {
             {. $toolName -h} | should throw 
+        }
+        
+        It 'Will have an empty string on the result.' {
+            $result | should be $null
         }
     }
 }
