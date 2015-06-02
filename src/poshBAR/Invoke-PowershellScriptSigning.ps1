e7113c0433bf281da5220e938d4a16fea50c6373
Chase Florell <chase_florell@transcanada.com>
2015-06-02 08:44:33 -0600

    [CF] updated script signing to use both the cert store or a pfx file path

--- src/poshBAR/Invoke-PowershellScriptSigning.ps1
+++ src/poshBAR/Invoke-PowershellScriptSigning.ps1
@@ -3,38 +3,52 @@
         Converts a powershell script to UTF8 and then signs it against a certificate
 
     .EXAMPLE
-        Set-PowershellScriptSigning @("C:\temp\someposh.ps1", "C:\otherposh.ps1")
+        Invoke-PowershellScriptSigning @("C:\temp\someposh.ps1", "C:\otherposh.ps1")
 
     .EXAMPLE
-        Set-PowershellScriptSigning @("C:\temp\someposh.ps1", "C:\otherposh.ps1") "CurrentUser\My"
+        Invoke-PowershellScriptSigning @("C:\temp\someposh.ps1", "C:\otherposh.ps1") "CurrentUser\My"
+
+    .EXAMPLE
+        Invoke-PowershellScriptSigning @("C:\temp\someposh.ps1", "C:\otherposh.ps1") (Resolve-Path 'C:\path\to\my.pfx')
 
     .PARAMETER scripts
         An array of powershell scripts to be signed
 
-    .PARAMETER certPath
+    .PARAMETER certStorePath
         Path to the certificate directory
+
+    .PARAMETER pfxFilePath
+        File path to a .pfx certificate.
 
     .SYNOPSIS
         Converts a powershell script to UTF8 and then signs it against a certificate
 
     .NOTES
-        Nothing yet...
+        If using -pfxFilePath, be sure to use Resolve-Path and pass in the PathInfo object.
 #>
 function Invoke-PowershellScriptSigning{
-    [CmdletBinding()]
+    [CmdletBinding(DefaultParameterSetName='store')]
     param(
-        [parameter(Mandatory=$true,position=0)] [string[]] $scripts,
-        [parameter(Mandatory=$false,position=1)] [string] $certPath = "LocalMachine\My"
+        [parameter(Mandatory=$true, position=0, ParameterSetName='store')] [string[]] 
+        [parameter(Mandatory=$true, position=0, ParameterSetName='path')] [string[]] 
+        $scripts,
+
+        [parameter(Mandatory=$false, position=1, ParameterSetName='store')] [string] $certStorePath = 'LocalMachine\My',
+        [parameter(Mandatory=$false, position=1, ParameterSetName='path')] [Management.Automation.PathInfo] $pfxFilePath
     )
 
-    $cert = @(Get-ChildItem cert:$certPath -codesign)[0]
+    switch($PsCmdlet.ParameterSetName){
+        'store' {$cert = @(Get-ChildItem cert:$certStorePath -codesign)[0]; break }
+        'path' {$cert = Get-PfxCertificate $pfxFilePath}
+    }
+
     $scripts | ? {$_.EndsWith(".ps1") -or $_.EndsWith(".psm1")} | % {
         # just make sure the file encoding is UTF8
         [System.Io.File]::ReadAllText($_) | Out-File -FilePath $_ utf8 -force
 
         # sign the file with our POSH cert.
-        Set-AuthenticodeSignature $_ $cert
+        Set-AuthenticodeSignature -FilePath $_ -certificate $cert
     }
 }
 
-Set-Alias sign Invoke-PowershellScriptSigning
+Set-Alias signscripts Invoke-PowershellScriptSigning
