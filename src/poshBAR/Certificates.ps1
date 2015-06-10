be6b05757fc5c93703ace1ed89e27aaf8ca50bd8
Chase Florell <chase_florell@transcanada.com>
2015-06-10 12:13:26 -0600

    [CF] still debugging CI

--- src/poshBAR/Certificates.ps1
+++ src/poshBAR/Certificates.ps1
@@ -371,6 +371,7 @@ function New-PfxCertificate {
     $pfx = "$($certData.name).pfx"
 
     Push-Location $certData.path
+    Write-Warning $env:PATH
     Exec { openssl pkcs12 -export -inkey $certData.key -in $certData.crt -out $pfx -name $certData.name  -passout pass:$password} 
     Pop-Location
     
