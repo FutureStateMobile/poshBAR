3498e68cf8d7cc2e19e3121e472c1e1eb6291bfc
Chase Florell <chase_florell@transcanada.com>
2015-06-10 12:08:19 -0600

    [CF] still debugging CI

--- src/poshBAR/Certificates.ps1
+++ src/poshBAR/Certificates.ps1
@@ -112,7 +112,7 @@ function New-PrivateKeyAndCertificateSigningRequest{
     $csr = "$name.csr"
 
     Push-Location $outPath
-    Exec { openssl req -nodes -newkey rsa:2048 -keyout $key -out $csr -subj $subject } 'An error occurred while executing openssl'
+    Exec { openssl req -nodes -newkey rsa:2048 -keyout $key -out $csr -subj $subject } 
     Pop-Location
     
     Write-Output @{
@@ -165,7 +165,7 @@ function New-PrivateKey {
     $key = "$name.key"
 
     Push-Location $outPath
-    Exec {openssl genrsa -passout pass:$password -out $key 2048 -subj $subject -noverify } 'An error occurred while executing openssl'
+    Exec {openssl genrsa -passout pass:$password -out $key 2048 -subj $subject -noverify } 
     Pop-Location
     
     Write-Output @{
@@ -235,7 +235,7 @@ function New-CertificateSigningRequest {
     $csr = "$($certData.name).csr"
     
     Push-Location $certData.path
-    Exec { openssl req -new -key $certData.key -out $csr -subj $certData.subject } 'An error occurred while executing openssl'
+    Exec { openssl req -new -key $certData.key -out $csr -subj $certData.subject } 
     Pop-Location
     
     $certData.Add('csr',$csr)
@@ -299,7 +299,7 @@ function New-Certificate {
     $crt = "$($certData.name).crt"
 
     Push-Location $certData.path
-    Exec { openssl x509 -req -days 365 -in $certData.csr -signkey $certData.key -out $crt  -text -inform DER } 'An error occurred while executing openssl'
+    Exec { openssl x509 -req -days 365 -in $certData.csr -signkey $certData.key -out $crt  -text -inform DER } 
     Pop-Location
 
     $certData.Add('crt', $crt)
@@ -371,7 +371,7 @@ function New-PfxCertificate {
     $pfx = "$($certData.name).pfx"
 
     Push-Location $certData.path
-    Exec { openssl pkcs12 -export -inkey $certData.key -in $certData.crt -out $pfx -name $certData.name  -passout pass:$password} 'An error occurred while executing openssl'
+    Exec { openssl pkcs12 -export -inkey $certData.key -in $certData.crt -out $pfx -name $certData.name  -passout pass:$password} 
     Pop-Location
     
     $certData.Add('pfx', $pfx)
