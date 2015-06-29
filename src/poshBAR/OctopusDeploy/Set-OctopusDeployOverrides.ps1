4013988fd0796bb46c134bdc1fed859adccd2618
Chase Florell <chase_florell@transcanada.com>
2015-06-29 12:12:29 -0600

    [CF] enforced boolean conversion on admin overrides

--- src/poshBAR/OctopusDeploy/Set-OctopusDeployOverrides.ps1
+++ src/poshBAR/OctopusDeploy/Set-OctopusDeployOverrides.ps1
@@ -1,9 +1,9 @@
 param( [hashtable] $poshBAR )
 
 if ( $OctopusParameters ) { 
-    $poshBAR.DisableWindowsFeaturesAdministration = $OctopusParameters['poshBAR.DisableWindowsFeaturesAdministration']
-    $poshBAR.DisableChocolateyInstallationAdministration = $OctopusParameters['poshBAR.DisableChocolateyInstallationAdministration']
-    $poshBAR.DisableHostFileAdministration = $OctopusParameters['poshBAR.DisableHostFileAdministration']
+    $poshBAR.DisableWindowsFeaturesAdministration = [System.Convert]::ToBoolean($OctopusParameters['poshBAR.DisableWindowsFeaturesAdministration'])
+    $poshBAR.DisableChocolateyInstallationAdministration = [System.Convert]::ToBoolean($OctopusParameters['poshBAR.DisableChocolateyInstallationAdministration'])
+    $poshBAR.DisableHostFileAdministration = [System.Convert]::ToBoolean($OctopusParameters['poshBAR.DisableHostFileAdministration'])
 }
 
 return $poshBAR
