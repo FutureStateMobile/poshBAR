a0e51c49e3cadca39c05a0bcc9ab9d4fb1d20f0d
Chase Florell <chase_florell@transcanada.com>
2015-06-29 11:53:12 -0600

    [CF] forgot to add the octopus override for DisableHostFileAdministration

--- src/poshBAR/OctopusDeploy/Set-OctopusDeployOverrides.ps1
+++ src/poshBAR/OctopusDeploy/Set-OctopusDeployOverrides.ps1
@@ -1,8 +1,9 @@
 param( [hashtable] $poshBAR )
 
 if ( $OctopusParameters ) { 
-    $poshBAR.DisableWindowsFeaturesAdministration = $OctopusParameters["poshBAR.DisableWindowsFeaturesAdministration"]
-    $poshBAR.DisableChocolateyInstallationAdministration = $OctopusParameters["poshBAR.DisableChocolateyInstallationAdministration"]
+    $poshBAR.DisableWindowsFeaturesAdministration = $OctopusParameters['poshBAR.DisableWindowsFeaturesAdministration']
+    $poshBAR.DisableChocolateyInstallationAdministration = $OctopusParameters['poshBAR.DisableChocolateyInstallationAdministration']
+    $poshBAR.DisableHostFileAdministration = $OctopusParameters['poshBAR.DisableHostFileAdministration']
 }
 
 return $poshBAR
