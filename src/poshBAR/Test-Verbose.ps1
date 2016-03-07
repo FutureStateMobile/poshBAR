1c7e5d912e38bc5ea5df25f4bae9a994be681893
Ralph Bohnet <ralph_bohnet@transcanada.com>
2016-03-07 09:00:59 -0700

    Added Test-Verbose to determine the value of the -Verbose flag

---
+++ src/poshBAR/Test-Verbose.ps1
@@ -0,0 +1,18 @@
+﻿<#
+    .DESCRIPTION
+        Tests if this powershell responds to verbose messages
+
+    .EXAMPLE
+        Test-Verbose
+
+    .SYNOPSIS
+        Tests if this powershell has been called with '-Verbose'.  
+        This will detect the Verbose setting even if iherits from its parent scope.
+#>
+
+function Test-Verbose{
+    [CmdletBinding()]
+    param()
+
+    [System.Management.Automation.ActionPreference]::SilentlyContinue -ne $VerbosePreference
+}
